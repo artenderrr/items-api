f3c6f862d1d62b09c0d5237ba1085a753efdebc5
artender <artem1347929@gmail.com>
2024-12-23 15:35:42 +0300

    ♻️ Refactor item path manipulations

--- app/core.py
+++ app/core.py
@@ -13,16 +13,20 @@ class ItemManager:
         item_files = os.listdir(self.path_to_items)
         return [int(i.split(".")[0]) for i in item_files]
     
+    def get_item_file_path(self, item_id: int) -> str:
+        """ Generate item file path based on given ID """
+        return f"{self.path_to_items}/{item_id}.json"
+    
     def item_id_exists(self, item_id: int) -> bool:
         """ Check if an item with the specified ID exists """
-        item_file = f"{self.path_to_items}/{item_id}.json"
-        return os.path.isfile(item_file)
+        item_file_path = self.get_item_file_path(item_id)
+        return os.path.isfile(item_file_path)
 
     def get_item(self, item_id: int) -> Item | None:
         """ Retrieve item by its ID """
         try:
-            path_to_item = f"{self.path_to_items}/{item_id}.json"
-            with open(path_to_item, encoding="utf-8") as file:
+            item_file_path = self.get_item_file_path(item_id)
+            with open(item_file_path, encoding="utf-8") as file:
                 return Item(**json.load(file))
         except FileNotFoundError:
             return None
@@ -40,8 +44,8 @@ class ItemManager:
         """ Create new item from given model and return completion status """
         if self.item_id_exists(item_id):
             return False
-        new_item_file = f"{self.path_to_items}/{item_id}.json"
-        with open(new_item_file, "w") as file:
+        new_item_file_path = self.get_item_file_path(item_id)
+        with open(new_item_file_path, "w") as file:
             json_compatible_item_data = jsonable_encoder(item)
             json.dump(json_compatible_item_data, file, indent=4)
         return True
@@ -50,6 +54,6 @@ class ItemManager:
         """ Delete existing item and return completion status """
         if not self.item_id_exists(item_id):
             return False
-        item_file = f"{self.path_to_items}/{item_id}.json"
-        os.remove(item_file)
+        item_file_path = self.get_item_file_path(item_id)
+        os.remove(item_file_path)
         return True
