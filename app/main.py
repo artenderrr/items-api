4ddee581c7805c1742f338a7d93c37dfb9dcd98a
artender <artem1347929@gmail.com>
2024-12-24 12:13:55 +0300

    📝 Document tags and endpoints description

--- app/main.py
+++ app/main.py
@@ -3,10 +3,18 @@ from fastapi import Depends
 from app.security import verify_api_key
 from app.router import router
 
+tags_metadata = [
+    {
+        "name": "items",
+        "description": "Manage items."
+    }
+]
+
 app = FastAPI(
     title="Items API",
     summary="Simple CRUD API to manage items.",
     version="1.0.0",
+    openapi_tags=tags_metadata,
     dependencies=[Depends(verify_api_key)]
 )
 
--- app/router.py
+++ app/router.py
@@ -6,33 +6,33 @@ from app.core import ItemManager
 
 router = APIRouter(tags=["items"])
 
-@router.get("/items")
+@router.get("/items", summary="Retrieve all existing items")
 def get_items(category: str | None = None) -> list[Item]:
     items = ItemManager().get_items(category)
     return items
 
-@router.get("/items/{item_id}")
+@router.get("/items/{item_id}", summary="Retrieve specific item by its ID")
 def get_item(item_id: Annotated[int, Path(gt=0)]) -> Item:
     item = ItemManager().get_item(item_id)
     if item is None:
         raise HTTPException(status_code=404, detail="Item not found")
     return item
 
-@router.post("/items/{item_id}", status_code=201)
+@router.post("/items/{item_id}", status_code=201, summary="Create new item")
 def create_item(item_id: Annotated[int, Path(gt=0)], item: Item) -> Item:
     success = ItemManager().create_item(item_id, item)
     if not success:
         raise HTTPException(status_code=409, detail="Item with specified ID already exists")
     return item
 
-@router.put("/items/{item_id}")
+@router.put("/items/{item_id}", summary="Modify existing item")
 def update_item(item_id: Annotated[int, Path(gt=0)], updated_item_fields: UpdatedItemFields) -> Item:
     success = ItemManager().update_item(item_id, updated_item_fields)
     if not success:
         raise HTTPException(status_code=404, detail="Item with specified ID doesn't exist")
     return cast(Item, ItemManager().get_item(item_id))
 
-@router.delete("/items/{item_id}", status_code=204)
+@router.delete("/items/{item_id}", status_code=204, summary="Remove existing item")
 def delete_item(item_id: Annotated[int, Path(gt=0)]) -> None:
     success = ItemManager().delete_item(item_id)
     if not success:
