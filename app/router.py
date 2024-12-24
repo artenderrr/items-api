c87a82b9bc5c86e6f5a521b332ad55a6f2c35124
artender <artem1347929@gmail.com>
2024-12-24 13:22:36 +0300

    ♻️ Reduce repetition

--- app/router.py
+++ app/router.py
@@ -7,34 +7,36 @@ from app.responses import item_not_found, item_already_exists
 
 router = APIRouter(tags=["items"])
 
+ValidItemID = Annotated[int, Path(gt=0)]
+
 @router.get("/items", summary="Retrieve all existing items")
 def get_items(category: str | None = None) -> list[Item]:
     items = ItemManager().get_items(category)
     return items
 
 @router.get("/items/{item_id}", summary="Retrieve specific item by its ID", responses={404: item_not_found})
-def get_item(item_id: Annotated[int, Path(gt=0)]) -> Item:
+def get_item(item_id: ValidItemID) -> Item:
     item = ItemManager().get_item(item_id)
     if item is None:
         raise HTTPException(status_code=404, detail="Item with specified ID doesn't exist")
     return item
 
 @router.post("/items/{item_id}", status_code=201, summary="Create new item", responses={409: item_already_exists})
-def create_item(item_id: Annotated[int, Path(gt=0)], item: Item) -> Item:
+def create_item(item_id: ValidItemID, item: Item) -> Item:
     success = ItemManager().create_item(item_id, item)
     if not success:
         raise HTTPException(status_code=409, detail="Item with specified ID already exists")
     return item
 
 @router.put("/items/{item_id}", summary="Modify existing item", responses={404: item_not_found})
-def update_item(item_id: Annotated[int, Path(gt=0)], updated_item_fields: UpdatedItemFields) -> Item:
+def update_item(item_id: ValidItemID, updated_item_fields: UpdatedItemFields) -> Item:
     success = ItemManager().update_item(item_id, updated_item_fields)
     if not success:
         raise HTTPException(status_code=404, detail="Item with specified ID doesn't exist")
     return cast(Item, ItemManager().get_item(item_id))
 
 @router.delete("/items/{item_id}", status_code=204, summary="Remove existing item", responses={404: item_not_found})
-def delete_item(item_id: Annotated[int, Path(gt=0)]) -> None:
+def delete_item(item_id: ValidItemID) -> None:
     success = ItemManager().delete_item(item_id)
     if not success:
         raise HTTPException(status_code=404, detail="Item with specified ID doesn't exist")
